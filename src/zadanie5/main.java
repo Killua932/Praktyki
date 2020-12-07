38bb70971b22448371aeee7d7d6d3c5235124728
Killua932 <nikodem.lucewicz@psr.gda.pl>
2020-12-07 12:22:58 +0100

    nadrabianie

--- src/zadanie5/main.java
+++ src/zadanie5/main.java
@@ -1,6 +1,6 @@
 package zadanie5;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         car bwm = new car("BMW",  6.5F) ;
         System.out.println(bwm.getModel());
